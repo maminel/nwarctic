ab17706e08487b4d226201aa97ca0c11c4626224
Chris Maxwell <cmaxwell@ojala.com>
2014-10-13 17:24:16 -0700

    Add drop-down navigation.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,22 @@
+// ===========================
+// Drop-down Navigation
+// ===========================
+
+$('nav li ul').hide().removeClass('fallback');
+$('nav li').hover(
+  function () {
+    $('ul', this).stop().slideDown(200);
+  },
+  function () {
+    $('ul', this).stop().slideUp(200);
+  }
+);
+
+
+// ===========================
+// Carousel
+// ===========================
+
 $(function() {
     $('#carousel_atc').carouFredSel({
         width: "100%",
