109ed30c612f490242fc00e18df3e1066be2b9dd
Chris Maxwell <cmaxwell@ojala.com>
2014-11-03 20:50:46 -0800

    Work on hero image and slider JavaScript.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -20,26 +20,19 @@ $('nav li').hover(
 $(document).ready(function() {
 
     $('.iosslider').iosSlider({
-        scrollbar            : true,
-        scrollbarHide        : false,
-        scrollbarLocation    : 'bottom',
-        scrollbarHeight      : '6px',
-        // scrollbarBackground  : 'url(_img/some-img.png) repeat 0 0',
-        scrollbarBackground  : '#fff',
-        scrollbarBorder      : '10px solid red',
-        // scrollbarMargin      : '0 30px 16px 30px',
-        // scrollbarOpacity     : '0.75',
-        // scrollbarContainer   : '.sliderContainer .scrollbarContainer',
         snapToChildren       : true,
+        snapSlideCenter      : true,
         desktopClickDrag     : true,
         keyboardControls     : true,
-        // responsiveSlideWidth : true,
-        infiniteSlider       : true,
-        // snapSlideCenter      : true,
-        // autoSlide            : true,
+        // responsiveSlideContainer : true,
+        // responsiveSlides     : true,
         navSlideSelector     : $('.slider-buttons .button'),
         navPrevSelector      : $('.prev-slide'),
         navNextSelector      : $('.next-slide'),
+        // autoSlide            : true,
+        // autoSlideTimer       : 5000,
+        // autoSlideHoverPause  : true,
+        infiniteSlider       : true,
         onSlideChange        : slideContentChange,
         onSlideComplete      : slideContentComplete,
         onSliderLoaded       : slideContentLoaded
@@ -52,11 +45,9 @@ $(document).ready(function() {
 
     }
 
-    function slideContentComplete(args) {
+    function slideContentLoaded(args) {
 
-        if(!args.slideChanged) return false;
-
-        $(args.sliderObject).find('.fade1, .fade2, .fade3, .swish1, .swish2').attr('style', '');
+        $(args.sliderObject).find('.fade1, .fade2').attr('style', '');
 
         $(args.currentSlideObject).find('.fade1').delay(400).animate({
             opacity: '1'
@@ -66,23 +57,11 @@ $(document).ready(function() {
             opacity: '1'
         }, 1000, 'easeOutQuint');
 
-        $(args.currentSlideObject).find('.fade3').delay(1200).animate({
-            opacity: '1'
-        }, 1400, 'easeOutQuint');
-
-        $(args.currentSlideObject).find('.swish1').animate({
-            left: '125px',
-            opacity: '1'
-        }, 1000, 'easeOutQuint');
-
-        $(args.currentSlideObject).find('.swish2').delay(1500).animate({
-            left: '125px',
-            opacity: '1'
-        }, 1000, 'easeOutQuint');
-
     }
 
-    function slideContentLoaded(args) {
+    function slideContentComplete(args) {
+
+        if(!args.slideChanged) return false;
 
         $(args.sliderObject).find('.fade1, .fade2, .fade3, .swish1, .swish2').attr('style', '');
 
