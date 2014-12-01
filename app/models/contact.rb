31743913eacdff5d92374613694659e8598351ad
Chris Maxwell <cmaxwell@ojala.com>
2014-12-01 10:16:46 -0800

    Refactor inquire form and model.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,24 +1,22 @@
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :name, validate: true
+  attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  attribute :nickname,  :captcha  => true
-  attribute :adress
-   attribute  :street_po
-    attribute :city
-    attribute :state
-    attribute :zip
-    attribute :telephone
-   attribute  :email
-    attribute :name_hs
-    attribute :location_hs
-    attribute :enrolled
-    attribute :interest
-    attribute :Eentrymonth 
-    attribute :Eentryyear
-    
-   attribute :graduateyear
-   
+  attribute :nickname, captcha: true
+  attribute :street_1
+  attribute :street_2
+  attribute :city
+  attribute :state
+  attribute :zip
+  attribute :telephone
+  attribute :email
+  attribute :hs_name
+  attribute :hs_location
+  attribute :enrolled
+  attribute :interest
+  attribute :entry_month
+  attribute :entry_year
+  attribute :grad_year
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
